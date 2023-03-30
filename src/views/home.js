e0b7f851641325e5c92276b10b9017cb34f1a0f0
Anuraag <anuraagmazumdar97@gmail.com>
2023-03-30 20:19:25 +0530

    Integrate spreadsheet form

--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
-import Navbar from '../components/navbar'
-import Slide from '../components/slide'
-import Footer from '../components/footer'
-import Copyright from '../components/copyright'
-import './home.css'
-import { width } from '@mui/system'
+import Navbar from "../components/navbar";
+import Slide from "../components/slide";
+import Footer from "../components/footer";
+import Copyright from "../components/copyright";
+import GetStarted from "../components/forms/get-started";
+import "./home.css";
+import { Box } from "@mui/material";
 
 const Home = (props) => {
   return (
@@ -49,7 +50,7 @@ const Home = (props) => {
                 <span className="home-text03">
                   <span
                     dangerouslySetInnerHTML={{
-                      __html: ' ',
+                      __html: " ",
                     }}
                   />
                 </span>
@@ -61,23 +62,8 @@ const Home = (props) => {
                 The one-stop education portal where we don’t just educate, we
                 elevate!
               </h2>
-              <div className="home-input-container">
-                <div className="home-container1 input">
-                  <svg
-                    viewBox="0 0 804.5714285714286 1024"
-                    className="home-icon"
-                  >
-                    <path d="M804.571 708.571c0 20.571-9.143 60.571-17.714 79.429-12 28-44 46.286-69.714 60.571-33.714 18.286-68 29.143-106.286 29.143-53.143 0-101.143-21.714-149.714-39.429-34.857-12.571-68.571-28-100-47.429-97.143-60-214.286-177.143-274.286-274.286-19.429-31.429-34.857-65.143-47.429-100-17.714-48.571-39.429-96.571-39.429-149.714 0-38.286 10.857-72.571 29.143-106.286 14.286-25.714 32.571-57.714 60.571-69.714 18.857-8.571 58.857-17.714 79.429-17.714 4 0 8 0 12 1.714 12 4 24.571 32 30.286 43.429 18.286 32.571 36 65.714 54.857 97.714 9.143 14.857 26.286 33.143 26.286 50.857 0 34.857-103.429 85.714-103.429 116.571 0 15.429 14.286 35.429 22.286 49.143 57.714 104 129.714 176 233.714 233.714 13.714 8 33.714 22.286 49.143 22.286 30.857 0 81.714-103.429 116.571-103.429 17.714 0 36 17.143 50.857 26.286 32 18.857 65.143 36.571 97.714 54.857 11.429 5.714 39.429 18.286 43.429 30.286 1.714 4 1.714 8 1.714 12z"></path>
-                  </svg>
-                  <input
-                    type="text"
-                    placeholder="Your phone number..."
-                    className="home-textinput3 input"
-                  />
-                </div>
-                <button className="home-button button-primary button">
-                  Get started
-                </button>
+              <div style={{ width: "100%" }}>
+                <GetStarted />
               </div>
               <div className="home-features-container">
                 <div className="home-feature">
@@ -154,7 +140,7 @@ const Home = (props) => {
                 all your problems.
                 <span
                   dangerouslySetInnerHTML={{
-                    __html: ' ',
+                    __html: " ",
                   }}
                 />
               </span>
@@ -189,7 +175,7 @@ const Home = (props) => {
                 <span>
                   <span
                     dangerouslySetInnerHTML={{
-                      __html: ' ',
+                      __html: " ",
                     }}
                   />
                 </span>
@@ -216,7 +202,7 @@ const Home = (props) => {
                   resources out there.
                   <span
                     dangerouslySetInnerHTML={{
-                      __html: ' ',
+                      __html: " ",
                     }}
                   />
                 </span>
@@ -224,7 +210,7 @@ const Home = (props) => {
                 <span>
                   <span
                     dangerouslySetInnerHTML={{
-                      __html: ' ',
+                      __html: " ",
                     }}
                   />
                 </span>
@@ -268,7 +254,7 @@ const Home = (props) => {
                 <span>
                   <span
                     dangerouslySetInnerHTML={{
-                      __html: ' ',
+                      __html: " ",
                     }}
                   />
                 </span>
@@ -311,15 +297,14 @@ const Home = (props) => {
             ></Slide>
           </div>
         </div>
-        <hr style={{width: "90%", border: "solid 1px grey"}} />
+        <hr style={{ width: "90%", border: "solid 1px grey" }} />
         <div className="home-cta section-container">
           <div className="home-max-width6 max-content-container">
             <div className="home-content-container4">
               <h1 className="home-text55">
-                {/* <span className="home-text56">Get started today.</span>
-                <span className="home-text57"> </span> */}
                 <span className="home-text58">
-                  Get started today.<br/>
+                  Get started today.
+                  <br />
                   Try 2 free demo classes!
                 </span>
               </h1>
@@ -329,24 +314,9 @@ const Home = (props) => {
                 </span>
                 <br className="home-text61"></br>
               </span>
-              <div className="home-input-container1">
-                <div className="home-container2 input">
-                  <svg
-                    viewBox="0 0 804.5714285714286 1024"
-                    className="home-icon10"
-                  >
-                    <path d="M804.571 708.571c0 20.571-9.143 60.571-17.714 79.429-12 28-44 46.286-69.714 60.571-33.714 18.286-68 29.143-106.286 29.143-53.143 0-101.143-21.714-149.714-39.429-34.857-12.571-68.571-28-100-47.429-97.143-60-214.286-177.143-274.286-274.286-19.429-31.429-34.857-65.143-47.429-100-17.714-48.571-39.429-96.571-39.429-149.714 0-38.286 10.857-72.571 29.143-106.286 14.286-25.714 32.571-57.714 60.571-69.714 18.857-8.571 58.857-17.714 79.429-17.714 4 0 8 0 12 1.714 12 4 24.571 32 30.286 43.429 18.286 32.571 36 65.714 54.857 97.714 9.143 14.857 26.286 33.143 26.286 50.857 0 34.857-103.429 85.714-103.429 116.571 0 15.429 14.286 35.429 22.286 49.143 57.714 104 129.714 176 233.714 233.714 13.714 8 33.714 22.286 49.143 22.286 30.857 0 81.714-103.429 116.571-103.429 17.714 0 36 17.143 50.857 26.286 32 18.857 65.143 36.571 97.714 54.857 11.429 5.714 39.429 18.286 43.429 30.286 1.714 4 1.714 8 1.714 12z"></path>
-                  </svg>
-                  <input
-                    type="text"
-                    placeholder="Your phone number..."
-                    className="home-textinput4 input"
-                  />
-                </div>
-                <button className="home-button button-primary button">
-                  Get started
-                </button>
-              </div>
+              <Box sx={{ width: { sm: "85%", xs: "100%" } }}>
+                <GetStarted />
+              </Box>
               <div className="home-features-container1">
                 <div className="home-feature4">
                   <svg
@@ -405,12 +375,12 @@ const Home = (props) => {
             </div>
           </div>
         </div>
-      <hr style={{width: "90%", border: "solid 1px grey"}} />
+        <hr style={{ width: "90%", border: "solid 1px grey" }} />
       </main>
       <Footer rootClassName="footer-root-class-name"></Footer>
       <Copyright></Copyright>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
